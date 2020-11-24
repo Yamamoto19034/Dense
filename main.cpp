289904febd87982847cf48ba98413be437ea8534
Yamamoto19034 <hj-g19034@sist.ac.jp>
2020-11-24 12:27:59 +0900

    シーン遷移・FPS値の実装

--- main.cpp
+++ main.cpp
@@ -9,8 +9,51 @@
 #define GAME_HEIGHT	600	//画面の縦の大きさ
 #define GAME_COLOR	32	//画面のカラービット
 
-#define GAME_WINDOW_BAR	0	//タイトルバーはデフォルトにする
+#define GAME_WINDOW_BAR		0	//タイトルバーはデフォルトにする
 #define GAME_WINDOW_NAME	"GAME TITLE"	//ウィンドウのタイトル
+
+#define GAME_FPS			60  //FPSの数値
+
+//マウスのボタン
+#define MOUSE_BUTTON_CODE	129
+
+enum GAME_SCENE {
+	GAME_SCENE_START,
+	GAME_SCENE_PLAY,
+	GAME_SCENE_END,
+};  //ゲームのシーン
+
+//######グローバル変数######
+int StartTimeFps;					//測定開始時刻
+int CountFps;					//カウンタ
+float CalcFps;					//計算結果
+int SampleNumFps = GAME_FPS;	//平均をとるサンプル数
+
+//キーボードの入力を取得
+char AllKeyState[256] = { 0 };
+char OldAllKeyState[256] = { 0 };
+
+int GameScene;					//ゲームシーンを管理
+
+//######プロトタイプ宣言######
+VOID MY_FPS_UPDATE(VOID);			//FPS値を計測、更新する
+VOID MY_FPS_DRAW(VOID);				//FPS値を描画する
+VOID MY_FPS_WAIT(VOID);				//FPS値を計測し、待つ
+
+VOID MY_ALL_KEYDOWN_UPDATE(VOID);	//キーの入力状態を更新する
+BOOL MY_KEY_DOWN(int);				//キーを押しているか、キーコードで判断する
+
+VOID MY_START(VOID);				//スタート画面
+VOID MY_START_PROC(VOID);			//スタート画面の処理
+VOID MY_START_DRAW(VOID);			//スタート画面の描画
+
+VOID MY_PLAY(VOID);					//プレイ画面
+VOID MY_PLAY_PROC(VOID);			//プレイ画面の処理
+VOID MY_PLAY_DRAW(VOID);			//プレイ画面の描画
+
+VOID MY_END(VOID);					//エンド画面
+VOID MY_END_PROC(VOID);				//エンド画面の処理
+VOID MY_END_DRAW(VOID);				//エンド画面の描画
 
 //########## プログラムで最初に実行される関数 ##########
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
@@ -26,28 +69,215 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 	int DrawX = 0;	//表示位置X
 	int DrawY = 0;	//表示位置Y
 
-	//▼▼▼▼▼ プログラム追加ここから ▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼
+	GameScene = GAME_SCENE_START;	//ゲームシーンはスタート画面から
+
 	SetDrawScreen(DX_SCREEN_BACK);	//Draw系関数は裏画面に描画
 
 	//無限ループ
 	while (TRUE)
 	{
 		if (ProcessMessage() != 0) { break; }	//メッセージ処理の結果がエラーのとき、強制終了
-
 		if (ClearDrawScreen() != 0) { break; }	//画面を消去できなかったとき、強制終了
 
-		DrawString(DrawX, DrawY, "Hello World", GetColor(255, 255, 255));	//文字を描画
+		MY_ALL_KEYDOWN_UPDATE();				//押しているキー状態を取得
 
-		ScreenFlip();		//モニタのリフレッシュレートの速さで裏画面を再描画
+		MY_FPS_UPDATE();						//FPSの処理(更新)
+
+		//シーンごとに処理を行う
+		switch (GameScene)
+		{
+		case GAME_SCENE_START:
+			MY_START();
+			break;
+		case GAME_SCENE_PLAY:
+			MY_PLAY();
+			break;
+		case GAME_SCENE_END:
+			MY_END();
+			break;
+		}
+
+		MY_FPS_DRAW();							//FPSの処理(描画)
+
+		ScreenFlip();							//モニタのリフレッシュレートの速さで裏画面を再描画
+
+		MY_FPS_WAIT();							//FPSの処理(待つ)
 	}
-	//▲▲▲▲▲ プログラム追加ここまで ▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲
-
-	//▽▽▽▽▽ プログラム削除ここから ▽▽▽▽▽▽▽▽▽▽▽▽▽▽▽▽▽▽▽▽
-	//DrawString(DrawX, DrawY, "Hello World", GetColor(255, 255, 255));	//文字を描画
-	//WaitKey();	//キー入力待ち
-	//△△△△△ プログラム削除ここまで △△△△△△△△△△△△△△△△△△△△
 
 	DxLib_End();	//ＤＸライブラリ使用の終了処理
 
 	return 0;
+}
+
+//########## FPS値を計測、更新する関数 ##########
+VOID MY_FPS_UPDATE(VOID)
+{
+	if (CountFps == 0) //1フレーム目なら時刻を記憶
+	{
+		StartTimeFps = GetNowCount();
+	}
+
+	if (CountFps == SampleNumFps) //60フレーム目なら平均を計算
+	{
+		int now = GetNowCount();
+		//現在の時間から、0フレーム目の時間を引き、FPSの数値で割る＝1FPS辺りの平均時間が計算される
+		CalcFps = 1000.f / ((now - StartTimeFps) / (float)SampleNumFps);
+		CountFps = 0;
+		StartTimeFps = now;
+	}
+	CountFps++;
+	return;
+}
+
+//########## FPS値を描画する関数 ##########
+VOID MY_FPS_DRAW(VOID)
+{
+	//文字列を描画
+	DrawFormatString(0, GAME_HEIGHT - 20, GetColor(255, 255, 255), "FPS:%.1f", CalcFps);
+	return;
+}
+
+//########## FPS値を計測し、待つ関数 ##########
+VOID MY_FPS_WAIT(VOID)
+{
+	int resultTime = GetNowCount() - StartTimeFps;					//かかった時間
+	int waitTime = CountFps * 1000 / GAME_FPS - resultTime;	//待つべき時間
+
+	if (waitTime > 0)		//指定のFPS値よりも早い場合
+	{
+		WaitTimer(waitTime);	//待つ
+	}
+	return;
+}
+
+//########## キーの入力状態を更新する関数 ##########
+VOID MY_ALL_KEYDOWN_UPDATE(VOID)
+{
+	char TempKey[256];  //一時的に、現在のキーの入力状態を格納する
+
+	//直前のキー入力を取っておく
+	for (int i = 0; i < 256; i++)
+	{
+		OldAllKeyState[i] = AllKeyState[i];
+	}
+
+	GetHitKeyStateAll(TempKey);		//全てのキーの入力状態を得る
+
+	for (int i = 0; i < 256; i++)
+	{
+		if (TempKey[i] != 0)		//押されているキーのキーコードの押しているとき
+		{
+			AllKeyState[i]++;		//押されている
+		}
+		else
+		{
+			AllKeyState[i] = 0;		//押されていない
+		}
+	}
+
+	return;
+}
+
+//キーを押しているか、キーコードで判断する
+BOOL MY_KEY_DOWN(int KEY_INPUT_)
+{
+	//キーコードのキーを押している時
+	if (AllKeyState[KEY_INPUT_] != 0)
+	{
+		return TRUE;	//キーを押している
+	}
+	else
+	{
+		return FALSE;	//キーを押していない
+	}
+}
+
+//スタート画面
+VOID MY_START(VOID)
+{
+	MY_START_PROC();	//スタート画面の処理
+	MY_START_DRAW();	//スタート画面の描画
+
+	return;
+}
+
+//スタート画面の処理
+VOID MY_START_PROC(VOID)
+{
+	//エンターキー押したら、プレイシーンへ移動する
+	if (MY_KEY_DOWN(KEY_INPUT_RETURN) == TRUE)
+	{
+		GameScene = GAME_SCENE_PLAY;
+	}
+
+	return;
+}
+
+//スタート画面の描画
+VOID MY_START_DRAW(VOID)
+{
+	DrawBox(10, 10, GAME_WIDTH - 10, GAME_HEIGHT - 10, GetColor(255, 0, 0), TRUE);
+	DrawString(0, 0, "スタート画面(エンターキーを押してください)", GetColor(255, 255, 255));
+
+	return;
+}
+
+//プレイ画面
+VOID MY_PLAY(VOID)
+{
+	MY_PLAY_PROC();	//スタート画面の処理
+	MY_PLAY_DRAW();	//スタート画面の描画
+
+	return;
+}
+
+//プレイ画面の処理
+VOID MY_PLAY_PROC(VOID)
+{
+	//スペースキー押したら、エンドシーンへ移動する
+	if (MY_KEY_DOWN(KEY_INPUT_SPACE) == TRUE)
+	{
+		GameScene = GAME_SCENE_END;
+	}
+
+	return;
+}
+
+//プレイ画面の描画
+VOID MY_PLAY_DRAW(VOID)
+{
+	DrawBox(10, 10, GAME_WIDTH - 10, GAME_HEIGHT - 10, GetColor(0, 255, 0), TRUE);
+	DrawString(0, 0, "プレイ画面(スペースキーを押してください)", GetColor(255, 255, 255));
+
+	return;
+}
+
+//エンド画面
+VOID MY_END(VOID)
+{
+	MY_END_PROC();	//スタート画面の処理
+	MY_END_DRAW();	//スタート画面の描画
+
+	return;
+}
+
+//エンド画面の処理
+VOID MY_END_PROC(VOID)
+{
+	//エスケープキー押したら、スタートシーンへ移動する
+	if (MY_KEY_DOWN(KEY_INPUT_ESCAPE) == TRUE)
+	{
+		GameScene = GAME_SCENE_START;
+	}
+
+	return;
+}
+
+//エンド画面の描画
+VOID MY_END_DRAW(VOID)
+{
+	DrawBox(10, 10, GAME_WIDTH - 10, GAME_HEIGHT - 10, GetColor(0, 0, 255), TRUE);
+	DrawString(0, 0, "エンド画面(エスケープキーを押してください)", GetColor(255, 255, 255));
+
+	return;
 }
