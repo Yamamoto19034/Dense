ef49b932792de58029043bbe741985d74a55aa4d
Yamamoto19034 <hj-g19034@sist.ac.jp>
2020-11-25 22:19:29 +0900

    画面の大きさを変更

--- main.cpp
+++ main.cpp
@@ -5,9 +5,9 @@
 #include "DxLib.h"
 
 //########## マクロ定義 ##########
-#define GAME_WIDTH	800	//画面の横の大きさ
-#define GAME_HEIGHT	600	//画面の縦の大きさ
-#define GAME_COLOR	32	//画面のカラービット
+#define GAME_WIDTH	1100	//画面の横の大きさ
+#define GAME_HEIGHT	750		//画面の縦の大きさ
+#define GAME_COLOR	32		//画面のカラービット
 
 #define GAME_WINDOW_BAR		0	//タイトルバーはデフォルトにする
 #define GAME_WINDOW_NAME	"GAME TITLE"	//ウィンドウのタイトル
